fc0cbc84e5902b3a6e097568be93fb9a71be4094
Cesar Verne <64318160+jonasMapuche@users.noreply.github.com>
2023-04-12 11:53:17 -0300

    Criação das classes e layouts dos fragmentos setting, home e bot.

---
+++ Letter/Views/HomeFragment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Letter.Views
+{
+    [Obsolete]
+    public class HomeFragment : Fragment
+    {
+        public int PlayId => Arguments.GetInt("current_play_id", 0);
+
+        public static HomeFragment NewInstance(int playId)
+        {
+            var bundle = new Bundle();
+            bundle.PutInt("current_play_id", playId);
+            return new HomeFragment { Arguments = bundle };
+        }
+
+        [Obsolete]
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.Inflate(Resource.Layout.fragment_home, container, false);
+           
+        }
+
+    }
+}
